ac92a638d663c7cd5d6cfefd17dfb1292c319375
sreekanth <sreestarta@gmail.com>
2019-06-10 22:14:55 -0500

    Copy on Write
    Value Types Copy on Write

--- StructCopyOnWrite/ViewController.swift
+++ StructCopyOnWrite/ViewController.swift
@@ -12,9 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        strucCopyOnWrite()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func strucCopyOnWrite() {
+        
+        var jamie = Person(details: Person.Details(address: "CA"), name: "jamie")
+        var jamie1 = jamie
+        
+        print("addres of jamie \(address(o: &jamie1))")
+        print("addres of jamie1 \(address(o: &jamie))")
+        jamie1.details = Person.Details(address: "CA")
+        
+        print("addres of jamie1 \(address(o: &jamie1))")
 
+    }
 
+    func address(o: UnsafePointer<Void>) -> Int {
+        return unsafeBitCast(o, to: Int.self)
+    }
+    
+    func addressHeap<T: AnyObject>(o: T) -> Int {
+        return unsafeBitCast(o, to: Int.self)
+    }
 }
+
+struct Person: Equatable {
+    struct Details: Equatable {
+        var address: String
+    }
+    var details: Details
+    var name: String
+}
+
+
 
